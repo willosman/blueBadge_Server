7ac7c159e43d3852a445fc90ec0638731c80dc2c
William Osman <wosman007@gmail.com>
2021-02-16 21:44:18 -0500

    adding controllers and models

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,4 +1,56 @@
 const router = require('express').Router();
 const User = require('../db').import('../models/user');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
+const user = require('../models/user');
+
+
+router.post('/register', function(req, res) {
+    User.create({email: req.body.user.email, password: bcrypt.hashSync(req.body.user.password, 13), firstName: req.body.user.firstName, zipcode: req.body.user.zipcode})
+    .then(function registrationSuccess(user){
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET,
+             {expiresIn: 60 * 60 * 24})
+        res.json({
+            user: user, 
+            message: "You have successfully registered for iPlant!",
+            sessionToken: token
+        })
+    })
+    .catch(err => res.status(500).json({error: err}))
+})
+
+router.post("/login", function(req, res) {
+    User.findOne({
+        where: {
+            email: req.body.user.email,
+        }
+    })
+    .then(function loginSuccess(user){
+        if(user){
+            bcrypt.compare(
+                req.body.user.password, user.password, 
+                function(err, matches){
+                    if(matches){
+                        let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {
+                            expiresIn: 60 * 60 * 24
+                        })
+                        res.status(200).json({
+                            user: user, 
+                            message: "Welcome!", 
+                            sessionToken: token
+                        })
+                    }
+                    else{
+                        res.status(502).send({error: "login failed"})
+                    }
+                }
+            )
+        }
+        else{
+            res.status(500).json({error: "user doesn't exist"})
+    }})
+    .catch(err => res.status(500).json({error: err}))
+    })
+
 
 module.exports = router;
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,20 @@
 module.exports = (sequelize, DataTypes) =>  {
     const User = sequelize.define('user', {
-        username: {
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true
         },
         password: {
             type: DataTypes.STRING,
+            allowNull: false
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        zipcode: {
+            type: DataTypes.INTEGER,
             allowNull: false
         }
     })
