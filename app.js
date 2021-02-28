1d1d6029b803478a3eee9c6d302ae1e73c0aab57
Bonita Scott <bonitalscott@yahoo.com>
2021-02-28 16:21:25 -0500

    updating server with dynamic port

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.use(express.json())
 app.use('/user', user);
 app.use('/plants', plants);
 
-app.listen(3000, function(){
-    console.log('App is listening on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log(`App is listening on port ${process.env.PORT}`);
+    
+// app.listen(3000, function(){
+//     console.log('App is listening on port 3000')
 });
