d8c8a14de2a47a24753ceff9e8f63f9b2a4592a5
Bonita Scott <bonitalscott@yahoo.com>
2021-03-04 22:19:18 -0500

    updating plantmodel to allow nulls in the note field

--- models/plant.js
+++ models/plant.js
@@ -1,32 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-    const Plants = sequelize.define("plants", {
-        trefle_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        common_name: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        scientific_name: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        image_url: {
-            type: DataTypes.STRING(2000),
-            allowNull: true
-        },
-        notes: {
-            type: DataTypes.STRING(2000),
-            allowNull: true
-        },
-        owner_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
+  const Plants = sequelize.define('plants', {
+    trefle_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    common_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    scientific_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image_url: {
+      type: DataTypes.STRING(2000),
+      allowNull: true
+    },
+    notes: {
+      type: DataTypes.STRING(2000),
+      allowNull: true
+    },
+    owner_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
 
-    })
-
-    return Plants;
-}
-
+  return Plants;
+};
