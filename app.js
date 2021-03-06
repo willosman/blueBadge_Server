49db29d722f1e78356fa2ad642e36cde04b4038b
Jonathan Cernero <cernero.jon@gmail.com>
2021-03-06 13:59:30 -0500

    stash

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-require("dotenv").config()
-let express = require('express');
-let app = express();
-const sequelize = require('./db');
+require('dotenv').config()
+let express = require('express')
+let app = express()
+const sequelize = require('./db')
 
-let plants = require('./controllers/plantcontroller');
-let user = require('./controllers/usercontroller');
+let plants = require('./controllers/plantcontroller')
+let user = require('./controllers/usercontroller')
 
-sequelize.sync();
-// sequelize.sync({force: true});
+sequelize.sync()
+// sequelize.sync({ force: true })
 
-app.use(require('./middleware/headers'));
+app.use(require('./middleware/headers'))
 
 app.use(express.json())
 
-app.use('/user', user);
-app.use('/plants', plants);
+app.use('/user', user)
+app.use('/plants', plants)
 
-app.listen(process.env.PORT, function(){
-    console.log(`App is listening on port ${process.env.PORT}`)
-});
+app.listen(process.env.PORT, function () {
+  console.log(`App is listening on port ${process.env.PORT}`)
+})
